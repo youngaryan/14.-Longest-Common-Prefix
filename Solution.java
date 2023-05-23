3afd6e0f77eb79490bcf5458db6d561a67a48d77
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-05-23 18:49:32 +0100

    done

--- Solution.java
+++ Solution.java
@@ -1,27 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        List<Character> list = new ArrayList<>();
-        List<Character> listC = new ArrayList<>();
+        if (strs.length==0) return "";
+        String result = strs[0];
 
-        for (int i = 0; i < strs.length; i++) {
-            list.add(strs[i].charAt(0));
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(result) !=0)
+                result = result.substring(0,result.length()-1);
         }
-        for (int i = 0; i < strs.length; i++) {
-            for (int j = i; j < strs.length; j++) {
-                if (list.get(i) != list.get(j))
-                    continue;
-                else{
-                    listC.add(list.get(i));
-                    break;
-                }
-            }
-        }
-        if (listC.isEmpty())return "";
-        throw new RuntimeException(String.valueOf(listC));
-
-        //return null;
+        return result;
     }
 }
--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-     String[] s =new String[]{"flower","flow","flight"};
+     String[] s =new String[]{"flower","dlow","flight"};
 
      Solution solution = new Solution();
         System.out.println(solution.longestCommonPrefix(s));
